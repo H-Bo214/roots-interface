0572227ceff3e75b03799e1b6478dea139099977
Horacio Borrego <hborrego365@gmail.com>
2020-10-22 18:02:08 -0600

    Set up TravisCI, update dev dependencies

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from '../logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Hello World!</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -17,7 +18,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
---
+++ src/App/App.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '../App/App';
+
+describe('App', () => {
+  it('should render App', () => {
+    render(
+      <App/>
+    )
+    const img = screen.getByRole('img', {name: 'logo'})
+    const title = screen.getByRole('heading', {name: 'Hello World!'})
+
+    expect(img).toBeInTheDocument()
+    expect(title).toBeInTheDocument();
+  })
+})

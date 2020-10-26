091c90db6a7972fc67f136bae19593f228007f13
Tyler Haglund <relyt4me@gmail.com>
2020-10-26 11:32:24 -0600

    Add examples of useState

--- src/components/NewRecipeForm/NewRecipeForm.js
+++ src/components/NewRecipeForm/NewRecipeForm.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NewRecipe.css';
 
 const NewRecipeForm = () => {
+  const [author, setAuthor] = useState('');
+  const [name, setName] = useState('');
+
   return (
     <form onSubmit={() => console.log('enter the thing')}>
       <h1>Add A Recipe</h1>
-      <input type='text' name='author' placeholder='Recipe Author' />
-      <input type='text' name='name' placeholder='Name of Recipe' />
+      <input type='text' name='author' placeholder='Recipe Author' onChange={(e) => setAuthor(e.target.value)} />
+      <input type='text' name='name' placeholder='Name of Recipe' onChange={(e) => setName(e.target.value)} />
       <textarea type='text' name='description' placeholder='Recipe Description' />
       <textarea type='text' name='instructions' placeholder='Recipe Instructions' />
       <input type='text' name='ingredient1' placeholder='Ingredient' />

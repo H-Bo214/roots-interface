667a6c8827cd5d13b2b2fc9db6deeb40cf7538f5
Tyler Haglund <relyt4me@gmail.com>
2020-10-28 13:13:07 -0600

    Style the recipe book to apear as a single book

--- src/components/RecipeBook/RecipeBook.js
+++ src/components/RecipeBook/RecipeBook.js
@@ -7,18 +7,31 @@ import { mockCookBook } from '../../mockData/mockData';
 const RecipeBook = (props) => {
   // make gql for the cookbook based on an id
   const listOfRecipes = mockCookBook.recipes.map((recipe) => {
-    return <li key={recipe.recipeId}>{recipe.recipeName}</li>;
+    return (
+      <Link to={`/single-recipe/${recipe.recipeId}`}>
+        <li key={recipe.recipeId}>{recipe.recipeName}</li>
+      </Link>
+    );
   });
 
   return (
-    <section>
-      <h1>{mockCookBook.name}</h1>
-      <p>{mockCookBook.author.userName}</p>
-      {listOfRecipes}
-      <Link to={`/add-recipe/${mockCookBook.id}`}>
-        <button>Add Recipe</button>
-      </Link>
-    </section>
+    <div className='recipe-book-page'>
+      <div className='recipe-book-wrapper'>
+        <section className='recipe-book'>
+          <div className='left-page'>
+            <h1 className='book-title'>{mockCookBook.name}</h1>
+          </div>
+          <div className='right-page'>
+            <p className='author'>Hosted by: {mockCookBook.author.userName}</p>
+
+            <article className='recipe-list'>{listOfRecipes}</article>
+            <Link to={`/add-recipe/${mockCookBook.id}`}>
+              <button>Add Recipe</button>
+            </Link>
+          </div>
+        </section>
+      </div>
+    </div>
   );
 };
 

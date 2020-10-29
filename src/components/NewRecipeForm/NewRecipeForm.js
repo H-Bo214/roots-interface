e306f517c020a7ecc05067cbcb2a43e4b52f777f
Horacio Borrego <hborrego365@gmail.com>
2020-10-29 11:10:14 -0600

    Update to make new ingredient fields have a unique id. Update syntax style

--- src/components/NewRecipeForm/NewRecipeForm.js
+++ src/components/NewRecipeForm/NewRecipeForm.js
@@ -34,7 +34,6 @@ const NewRecipeForm = () => {
     if (values.length > 1) {
       values.splice(index, 1)
       setIngredients(values)
-
     }
   }
   
@@ -42,12 +41,39 @@ const NewRecipeForm = () => {
     return ingredients.map((ingredient, index) =>{
       return <div key={index}>
         <div className='unit-measurement-parent'>
-          <label htmlFor='ingredient1'>Ingredient:</label>
-          <input name='name' label='ingredient-name' value={ingredient.name} onChange={e => handleChange(e, index)} className='ingredient'/>
-          <label htmlFor='unit1'>Unit:</label>
-          <input id='unit1' className='unit' type='text' name='unit' placeholder='Unit' value={ingredient.unit} onChange={e => handleChange(e, index)}/>
-          <label htmlFor='measurement1'>Measurement:</label>
-          <input id='measurement1' className='measurement' type='text' name='measurement' placeholder='Measurement' value={ingredient.measurement} onChange={e => handleChange(e, index)}/>
+          <label htmlFor={`ingredient${index}`}>Ingredient:</label>
+          <input 
+            maxLength='30' 
+            id={`ingredient${index}`}
+            name='name' 
+            className='ingredient'
+            value={ingredient.name} 
+            type='text'
+            placeholder='Ingredient name'
+            onChange={e => handleChange(e, index)} 
+          />
+          <label htmlFor={`unit${index}`}>Unit:</label>
+          <input 
+            maxLength='10' 
+            id={`unit${index}`} 
+            name='unit' 
+            className='unit' 
+            value={ingredient.unit} 
+            type='text' 
+            placeholder='Unit' 
+            onChange={e => handleChange(e, index)}
+          />
+          <label htmlFor={`measurement${index}`}>Measurement:</label>
+          <input 
+            maxLength='3' 
+            id={`measurement${index}`} 
+            name='measurement' 
+            className='measurement' 
+            value={ingredient.measurement} 
+            type='text'
+            placeholder='Measurement' 
+            onChange={e => handleChange(e, index)}
+          />
           <button onClick={() => handleAddIngredient()}>Ingredient + </button>
           <button onClick={() => handleRemoveIngredient(index)}>Ingredient - </button>
         </div> 
@@ -59,25 +85,57 @@ const NewRecipeForm = () => {
     <div>
       <div className='NewRecipeForm'>
         <form className='add-recipe-form' onSubmit={handleSubmit}>
-      <h1>Add a recipe</h1>
-      <div className='recipe-details-container'>
-          <div className='recipe-details'>
-            <label htmlFor='author'>Author:</label>
-            <input id="author" className='author' type='text' name='author' placeholder='Recipe Author' onChange={(e) => setAuthor(e.target.value)} />
-            <label htmlFor='recipe-name'>Name:</label>
-            <input id='recipe-name' className='recipe-name' type='text' name='name' placeholder='Name of Recipe' onChange={(e) => setName(e.target.value)} />
-            <label htmlFor='description'>Description:</label>
-            <textarea id='description' className='description' type='text' name='description' placeholder='Recipe Description' onChange={(e) => setDescription(e.target.value)} />
-            <label htmlFor='instructions'>Instructions:</label>
-            <textarea id='instructions' className='instructions' type='text' name='instructions' placeholder='Recipe Instructions' onChange={(e) => setInstructions(e.target.value)}/>
-            <div className='button-parent'>
-              <button className='add-recipe-button' type ='button' onClick={handleSubmit}>Add to my recipe book</button>
+          <h1>Add a recipe</h1>
+          <div className='recipe-details-container'>
+            <div className='recipe-details'>
+              <label htmlFor='author'>Author:</label>
+              <input 
+                id="author" 
+                className='author' 
+                type='text' 
+                name='author' 
+                placeholder='Recipe author' 
+                onChange={(e) => setAuthor(e.target.value)} 
+              />
+              <label htmlFor='recipe-name'>Name:</label>
+              <input 
+                id='recipe-name' 
+                className='recipe-name' 
+                type='text' 
+                name='name' 
+                placeholder='Name of recipe' 
+                onChange={(e) => setName(e.target.value)} 
+              />
+              <label htmlFor='description'>Description:</label>
+              <textarea 
+                id='description' 
+                className='description' 
+                type='text' 
+                name='description' 
+                placeholder='Recipe description' 
+                onChange={(e) => setDescription(e.target.value)} 
+              />
+              <label htmlFor='instructions'>Instructions:</label>
+              <textarea 
+                id='instructions' 
+                className='instructions' 
+                type='text' 
+                name='instructions' 
+                placeholder='Recipe instructions' 
+                onChange={(e) => setInstructions(e.target.value)}
+              />
+              <div className='button-parent'>
+                <button 
+                  className='add-recipe-button' 
+                  type ='button' 
+                  onClick={handleSubmit}>Add to my recipe book
+                </button>
+              </div>
+            </div>
+            <div className='ingredients-parents'>
+              {ingredientFields()}
             </div>
           </div>
-          <div className='ingredients-parents'>
-            {ingredientFields()}
-          </div>
-      </div>
         </form>
       </div>
     </div>

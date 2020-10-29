85dbd29bc306d91109d9e11eb71640e965116e33
Horacio Borrego <hborrego365@gmail.com>
2020-10-29 14:13:13 -0600

    Clean up syntax in BookTree and remove unnecessary import

--- src/components/BookTree/BookTree.js
+++ src/components/BookTree/BookTree.js
@@ -6,11 +6,47 @@ import AddCookBook from '../AddCookBook/AddCookBook';
 
 const BookTree = () => {
   const [user, setUser] = useState(mockUser);
+
   const userCookBooks = user.ownedBooks.map((book) => {
-    return <BookCard cookBookName={book.bookName} key={book.bookId} bookId={book.bookId} />;
+    return <BookCard 
+      cookBookName={book.bookName} 
+      key={book.bookId} 
+      bookId={book.bookId} 
+    />;
   });
 
   const addNewBook = (bookName) => {
+    const newBook = [...user.ownedBooks, { bookName }];
+    const updatedUser = {
+      name: user.name,
+      userId: user.userId,
+      ownedBooks: newBook,
+    };
+    setUser(updatedUser);
+  };
+
+  return (
+    <section className='BookTree'>
+      <div className='root-background'></div>
+      <div className='user-icon-container'>
+        <img 
+          src='https://image.flaticon.com/icons/png/512/17/17004.png' 
+          alt='user icon' 
+          className='user-img' 
+        />
+      </div>
+      <div className='user-cook-books'>
+        <AddCookBook 
+         addNewBook={addNewBook} 
+        />
+        {userCookBooks}
+      </div>
+    </section>
+  );
+};
+
+export default BookTree;
+
     // Will need to send a mutation request to BE to add/create a new cookbook // Endpoint name createCookbook //
     // format//
     //      {
@@ -19,39 +55,3 @@ const BookTree = () => {
     //          userId: user.userId,
     //          userName: user.name
     //       }
-    const newBook = [...user.ownedBooks, { bookName }];
-    const updatedUser = {
-      name: user.name,
-      userId: user.userId,
-      ownedBooks: newBook,
-    };
-
-    setUser(updatedUser);
-  };
-
-  //map over gotten user data to create multiple book cards
-  // const userCookBooks = mockUser.ownedBooks.map(book => {
-  //   return <BookCard
-  //   cookBookName={book.bookName}
-  //   key={book.bookId}
-  //   bookId={book.bookId}
-  //   />
-  // })
-  return (
-    <section className='BookTree'>
-      <div className='root-background'></div>
-      <div className='user-icon-container'>
-        <img src='https://image.flaticon.com/icons/png/512/17/17004.png' alt='user icon' className='user-img' />
-      </div>
-      {/* <div> */}
-      {/* <AddCookBook addNewBook={addNewBook}/> */}
-      {/* </div> */}
-      <div className='user-cook-books'>
-        <AddCookBook addNewBook={addNewBook} />
-        {userCookBooks}
-      </div>
-    </section>
-  );
-};
-
-export default BookTree;

131fbf6d7b55b7b69f3a1e62b5e33fbfbcfb35b9
Tyler Haglund <relyt4me@gmail.com>
2020-10-29 09:42:06 -0600

    Change up button and add dynamic transform

--- src/components/AddCookBook/AddCookBook.js
+++ src/components/AddCookBook/AddCookBook.js
@@ -1,31 +1,23 @@
-import React, { useState } from 'react' 
-import './AddCookBook.css'
+import React, { useState } from 'react';
+import './AddCookBook.css';
 
-const AddCookBook = ( { addNewBook } ) => {
+const AddCookBook = ({ addNewBook }) => {
   const [value, setValue] = useState('');
 
-  const handleSubmit = e => {
-    e.preventDefault()
+  const handleSubmit = (e) => {
+    e.preventDefault();
     if (!value) return;
-    addNewBook(value)
-    setValue('')
-  }
-
-
+    addNewBook(value);
+    setValue('');
+  };
 
   return (
-    <form className='cookbook-form'>
+    <form className='cookbook-form' onSubmit={handleSubmit}>
       <h3>Add a new cookbook</h3>
-      <input
-        type='text'
-        className='new-book-input'
-        placeholder='Cookbook name...'
-        value={value}
-        onChange={ e => setValue(e.target.value)}
-      />
-      <button type='button' onClick={handleSubmit}>ADD TO COOKBOOKS</button>
+      <input type='text' className='new-book-input' placeholder='Cookbook name...' value={value} onChange={(e) => setValue(e.target.value)} />
+      <button type='submit'>ADD TO COOKBOOKS</button>
     </form>
-  )
-}
+  );
+};
 
 export default AddCookBook;

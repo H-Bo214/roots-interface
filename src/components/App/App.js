9818b193a8b56e3e677093acf2cdaceb03880ec0
Tyler Haglund <relyt4me@gmail.com>
2020-10-31 13:41:11 -0600

    Start moving Queries to seperate file

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,15 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Nav from '../Nav/Nav';
 import BookTree from '../BookTree/BookTree';
 import RecipeBook from '../RecipeBook/RecipeBook';
 import SingleRecipe from '../SingleRecipe/SingleRecipe';
 
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
-
 import './App.css';
 import AddRecipe from '../AddRecipe/AddRecipe';
-// const client  = new ApolloClient({// uri:'your graphQL route'})
 
 const App = () => {
   return (
-    // <ApolloProvider client={/* should be 'client', no quotes */}>
-    // changed 'body' back to div due to console error. unable to have a body tag as a child of a div.
-    // There seem to be two routes to the same location.
     <div className='App'>
       <Switch>
         <Route
@@ -41,7 +34,6 @@ const App = () => {
       </Switch>
       <Nav />
     </div>
-    // </ApolloProvider>
   );
 };
 

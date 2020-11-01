e1274c75b6f44f6eda240bec75afbf367817bd61
Tyler Haglund <relyt4me@gmail.com>
2020-10-31 18:11:24 -0600

    Change data type for query to id

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
+//https://cors-anywhere.herokuapp.com/
 const client = new ApolloClient({
-  uri: 'heroku url',
+  uri: 'https://cors-anywhere.herokuapp.com/https://glacial-falls-21490.herokuapp.com/graphql',
   cache: new InMemoryCache()
 });
 

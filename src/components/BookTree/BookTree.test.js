9832e892eb36b8563070419b33b3370feff9f37f
Tyler Haglund <relyt4me@gmail.com>
2020-11-02 14:28:50 -0700

    Update syntax per style guides for BookTree

--- src/components/BookTree/BookTree.test.js
+++ src/components/BookTree/BookTree.test.js
@@ -2,79 +2,74 @@ import React from 'react';
 import { screen, render, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import BookTree from './BookTree';
-import { GET_USER_COOKBOOKS } from '../../queries/Queries'
-import { MockedProvider} from '@apollo/client/testing'
-
+import { GET_USER_COOKBOOKS } from '../../queries/Queries';
+import { MockedProvider } from '@apollo/client/testing';
 
 describe('BookTree', () => {
-  let mocks;
-  beforeEach(() => {
-    mocks = [
-      {
-        request: {
-          query: GET_USER_COOKBOOKS,
-          variables: {
-            id: '0',
-          },
-        },
-        result: {
-          data: {
-            "getUserCookbooks": [
-              {
-                "id": "1",
-                "title": "Neeru's Cookbook",
-              },
-              {
-                "id": "2",
-                "title": "Indian Food",
-              }
-            ]
-          }
-        },
-      },
-    ]
-  })
-  it('should render with while loading', () => {
-    let name = 'Joe'
-    let id = '0'
-    render(
-      <MemoryRouter>
-        <MockedProvider mocks={mocks}>
-          <BookTree
-            user={name}
-            id={id}
-          />
-        </MockedProvider>
-      </MemoryRouter>
-    );
+	let mocks;
+	beforeEach(() => {
+		mocks = [
+			{
+				request: {
+					query: GET_USER_COOKBOOKS,
+					variables: {
+						id: '0',
+					},
+				},
+				result: {
+					data: {
+						getUserCookbooks: [
+							{
+								id: '1',
+								title: "Neeru's Cookbook",
+							},
+							{
+								id: '2',
+								title: 'Indian Food',
+							},
+						],
+					},
+				},
+			},
+		];
+	});
+	it('should render with while loading', () => {
+		let name = 'Joe';
+		let id = '0';
+		render(
+			<MemoryRouter>
+				<MockedProvider mocks={mocks}>
+					<BookTree user={name} id={id} />
+				</MockedProvider>
+			</MemoryRouter>
+		);
 
-    const loading = screen.getByText('Loading...')
+		const loading = screen.getByText('Loading...');
 
-    expect(loading).toBeInTheDocument();
-  });
+		expect(loading).toBeInTheDocument();
+	});
 
-  it('should render correctly after loading', async () => {
-    let name = 'Joe'
-    let id = '0'
-    render(
-      <MemoryRouter>
-        <MockedProvider mocks={mocks}>
-          <BookTree
-            user={name}
-            id={id}
-          />
-        </MockedProvider>
-      </MemoryRouter>
-    );
+	it('should render correctly after loading', async () => {
+		let name = 'Joe';
+		let id = '0';
+		render(
+			<MemoryRouter>
+				<MockedProvider mocks={mocks}>
+					<BookTree user={name} id={id} />
+				</MockedProvider>
+			</MemoryRouter>
+		);
 
-    const userIcon = await waitFor(() => screen.getByAltText('user icon'));
-    const addCookBookButton = screen.getByRole('button', {name: 'Add to cookbooks'})
-    const cookbookOne = screen.getByText('Neeru\'s Cookbook');
-    const cookbookTwo = screen.getByText('Indian Food');
+		const userIcon = await waitFor(() => screen.getByAltText('user icon'));
+		const addCookBookButton = screen.getByRole('button', {
+			name: 'Add to cookbooks',
+		});
+		const cookbookOne = screen.getByText("Neeru's Cookbook");
+		const cookbookTwo = screen.getByText('Indian Food');
 
-    expect(userIcon).toBeInTheDocument();
-    expect(addCookBookButton).toBeInTheDocument();
-    expect(cookbookOne).toBeInTheDocument();
-    expect(cookbookTwo).toBeInTheDocument();
-  });
+		expect(userIcon).toBeInTheDocument();
+		expect(addCookBookButton).toBeInTheDocument();
+		expect(cookbookOne).toBeInTheDocument();
+		expect(cookbookTwo).toBeInTheDocument();
+	});
 });

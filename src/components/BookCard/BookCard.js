c34e14fea7c6df1689874ebec2408a328fe84e18
Tyler Haglund <relyt4me@gmail.com>
2020-11-01 17:57:30 -0700

    Remove the old card image

--- src/components/BookCard/BookCard.js
+++ src/components/BookCard/BookCard.js
@@ -1,16 +1,12 @@
 import React, { Fragment } from 'react';
 import './BookCard.css';
-import spiralBook from '../../assets/spiral-book.svg';
+import spiralBook from '../../assets/red-book.png';
 
 const BookCard = ({ cookBookName, bookId }) => {
   return (
     <Fragment>
         <section className='family-cookbook' >
           <h3>{cookBookName}</h3>
-          <img 
-            src={spiralBook}
-            alt='spiral book icon'
-          />
         </section>
     </Fragment>
   )

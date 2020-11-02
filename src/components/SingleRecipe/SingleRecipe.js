bf6872aa0a2e66f738e401a28f1616c056b4e340
Tyler Haglund <relyt4me@gmail.com>
2020-11-02 14:52:48 -0700

    Update syntax per style guides for SingleRecipe

--- src/components/SingleRecipe/SingleRecipe.js
+++ src/components/SingleRecipe/SingleRecipe.js
@@ -1,47 +1,47 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './SingleRecipe.css';
-import { useQuery } from '@apollo/client'
+import { useQuery } from '@apollo/client';
 import { GET_RECIPE } from '../../queries/Queries';
 
-const SingleRecipe = ({recipeId}) => {
-  const {loading, error, data} = useQuery(GET_RECIPE, {
-    variables: { id: recipeId },
-  })
+const SingleRecipe = ({ recipeId }) => {
+	const { loading, error, data } = useQuery(GET_RECIPE, {
+		variables: { id: recipeId },
+	});
 
-  // map over ingredients
-  let recipeIngredients;
-  if(data) recipeIngredients = data.getRecipe.ingredients.map((ingredient) => {
-    return (
-      <li key={ingredient.id}>
-        {ingredient.amount} {ingredient.unit} {ingredient.name}
-      </li>
-    );
-  });
+	let recipeIngredients;
+	if (data)
+		recipeIngredients = data.getRecipe.ingredients.map((ingredient) => {
+			return (
+				<li key={ingredient.id}>
+					{ingredient.amount} {ingredient.unit} {ingredient.name}
+				</li>
+			);
+		});
 
-  if(loading) return <h1>Loading...</h1>;
-  if(error) {
-    console.log(error);
-    return <h1>error</h1>;
-  }
+	if (loading) return <h1>Loading...</h1>;
+	if (error) {
+		console.log(error);
+		return <h1>error</h1>;
+	}
 
-  return (
-    <div className='recipe-page-background'>
-      <div className='recipe-page-paper'>
-        <article className='recipe-page-content'>
-          <div className='recipe-text'>
-            <h1>{data.getRecipe.title}</h1>
-            <br></br>
-            <p>{data.getRecipe.author}</p>
-            <p>{data.getRecipe.description}</p>
-            <br></br>
-            {recipeIngredients}
-            <br></br>
-            <p className='recipe-instructions'>{data.getRecipe.instructions}</p>
-          </div>
-        </article>
-      </div>
-    </div>
-  );
+	return (
+		<div className="recipe-page-background">
+			<div className="recipe-page-paper">
+				<article className="recipe-page-content">
+					<div className="recipe-text">
+						<h1>{data.getRecipe.title}</h1>
+						<br></br>
+						<p>{data.getRecipe.author}</p>
+						<p>{data.getRecipe.description}</p>
+						<br></br>
+						{recipeIngredients}
+						<br></br>
+						<p className="recipe-instructions">{data.getRecipe.instructions}</p>
+					</div>
+				</article>
+			</div>
+		</div>
+	);
 };
 
 export default SingleRecipe;
--- src/components/SingleRecipe/SingleRecipe.test.js
+++ src/components/SingleRecipe/SingleRecipe.test.js
@@ -2,88 +2,84 @@ import React from 'react';
 import { screen, render, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import SingleRecipe from './SingleRecipe';
-import { GET_RECIPE } from '../../queries/Queries'
-import { MockedProvider} from '@apollo/client/testing'
-
+import { GET_RECIPE } from '../../queries/Queries';
+import { MockedProvider } from '@apollo/client/testing';
 
 describe('Single Recipe', () => {
-  let mocks;
-  beforeEach(() => {
-    mocks = [
-      {
-        request: {
-          query: GET_RECIPE,
-          variables: {
-            id: '1',
-          },
-        },
-        result: {
-          data: {
-            "getRecipe": {
-              "author": "Michael Alex",
-              "description": "A hearty beef and potato stew",
-              "id": "1",
-              "ingredients": [
-                {
-                  "amount": 2,
-                  "id": "1",
-                  "name": "beef",
-                  "unit": "lbs"
-                },
-                {
-                  "amount": 3,
-                  "id": "2",
-                  "name": "potato",
-                  "unit": "lbs"
-                }
-              ],
-              "instructions": "Cook in a crockpot",
-              "title": "Campfire Stew"
-            }
-          }
-        },
-      },
-    ]
-  })
-  it('should render with loading while loading', () => {
-    render(
-      <MemoryRouter>
-        <MockedProvider mocks={mocks}>
-          <SingleRecipe
-            recipeId={'1'}
-          />
-        </MockedProvider>
-      </MemoryRouter>
-    );
+	let mocks;
+	beforeEach(() => {
+		mocks = [
+			{
+				request: {
+					query: GET_RECIPE,
+					variables: {
+						id: '1',
+					},
+				},
+				result: {
+					data: {
+						getRecipe: {
+							author: 'Michael Alex',
+							description: 'A hearty beef and potato stew',
+							id: '1',
+							ingredients: [
+								{
+									amount: 2,
+									id: '1',
+									name: 'beef',
+									unit: 'lbs',
+								},
+								{
+									amount: 3,
+									id: '2',
+									name: 'potato',
+									unit: 'lbs',
+								},
+							],
+							instructions: 'Cook in a crockpot',
+							title: 'Campfire Stew',
+						},
+					},
+				},
+			},
+		];
+	});
 
-    const loading = screen.getByText('Loading...')
+	it('should render with loading while loading', () => {
+		render(
+			<MemoryRouter>
+				<MockedProvider mocks={mocks}>
+					<SingleRecipe recipeId={'1'} />
+				</MockedProvider>
+			</MemoryRouter>
+		);
 
-    expect(loading).toBeInTheDocument();
-  });
+		const loading = screen.getByText('Loading...');
 
-  it('should render correctly after loading', async () => {
-    render(
-      <MemoryRouter>
-        <MockedProvider mocks={mocks}>
-          <SingleRecipe
-            recipeId={'1'}
-          />
-        </MockedProvider>
-      </MemoryRouter>
-    );
+		expect(loading).toBeInTheDocument();
+	});
 
-    const author = await waitFor(() => screen.getByText('Michael Alex'));
-    const description = screen.getByText('A hearty beef and potato stew');
-    const instructions = screen.getByText('Cook in a crockpot');
-    const title = screen.getByText('Campfire Stew');
-    const ingredientOne = screen.getByText('2 lbs beef');
-    const ingredientTwo = screen.getByText('3 lbs potato');
+	it('should render correctly after loading', async () => {
+		render(
+			<MemoryRouter>
+				<MockedProvider mocks={mocks}>
+					<SingleRecipe recipeId={'1'} />
+				</MockedProvider>
+			</MemoryRouter>
+		);
 
-    expect(author).toBeInTheDocument();
-    expect(description).toBeInTheDocument();
-    expect(instructions).toBeInTheDocument();
-    expect(title).toBeInTheDocument();
-    expect(ingredientOne).toBeInTheDocument();
-    expect(ingredientTwo).toBeInTheDocument();
-  });
+		const author = await waitFor(() => screen.getByText('Michael Alex'));
+		const description = screen.getByText('A hearty beef and potato stew');
+		const instructions = screen.getByText('Cook in a crockpot');
+		const title = screen.getByText('Campfire Stew');
+		const ingredientOne = screen.getByText('2 lbs beef');
+		const ingredientTwo = screen.getByText('3 lbs potato');
+
+		expect(author).toBeInTheDocument();
+		expect(description).toBeInTheDocument();
+		expect(instructions).toBeInTheDocument();
+		expect(title).toBeInTheDocument();
+		expect(ingredientOne).toBeInTheDocument();
+		expect(ingredientTwo).toBeInTheDocument();
+	});
 });

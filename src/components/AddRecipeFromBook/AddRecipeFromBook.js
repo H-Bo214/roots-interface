506a81ccc2163ad23efb2b94d606c09f777550a0
Tyler Haglund <relyt4me@gmail.com>
2020-11-03 11:01:45 -0700

    Comment the unused component out until extention

--- src/components/AddRecipeFromBook/AddRecipeFromBook.js
+++ src/components/AddRecipeFromBook/AddRecipeFromBook.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import '../AddRecipeFromBook/AddRecipeFromBook.css'
-/// This component is for future improvements. A user will be able to choose another family member's book and add a recipe from that book. Will require conditional rendering, choose book first, then render the recipe drop down.
-const AddRecipeFromBook = () => {
-  return (
-    <form className='book-form'>
-      <h3>Add from an existing cookbook</h3>
-      <label htmlFor='book-selection'>Select a book:</label>
-      <select id='book-selection' name='books'>
-        <option>Book1</option>
-        <option>Book2</option>
-      </select>
-      <label htmlFor='recipe-selection'>Select a recipe:</label>
-      <select id='recipe-selection' name='recipe'>
-        <option>Recipe1</option>
-        <option>Recipe2</option>
-      </select>
-      <button className='add-recipe-button' type ='button' onClick={() => console.log('add recipe')}>Add to my recipe book</button>
-    </form>
-  )
-}
+// import React from 'react'
+// import '../AddRecipeFromBook/AddRecipeFromBook.css'
+// /// This component is for future improvements. A user will be able to choose another family member's book and add a recipe from that book. Will require conditional rendering, choose book first, then render the recipe drop down.
+// const AddRecipeFromBook = () => {
+//   return (
+//     <form className='book-form'>
+//       <h3>Add from an existing cookbook</h3>
+//       <label htmlFor='book-selection'>Select a book:</label>
+//       <select id='book-selection' name='books'>
+//         <option>Book1</option>
+//         <option>Book2</option>
+//       </select>
+//       <label htmlFor='recipe-selection'>Select a recipe:</label>
+//       <select id='recipe-selection' name='recipe'>
+//         <option>Recipe1</option>
+//         <option>Recipe2</option>
+//       </select>
+//       <button className='add-recipe-button' type ='button' onClick={() => console.log('add recipe')}>Add to my recipe book</button>
+//     </form>
+//   )
+// }
 
-export default AddRecipeFromBook;
+// export default AddRecipeFromBook;

a63d57cf50877ace0411a167ebbca0cd0a1fa338
Tyler Haglund <relyt4me@gmail.com>
2020-11-02 19:17:26 -0700

    Update syntax per style guides for AddCookBook

--- src/components/AddCookBook/AddCookBook.js
+++ src/components/AddCookBook/AddCookBook.js
@@ -1,36 +1,36 @@
 import React, { useState } from 'react';
 import './AddCookBook.css';
-import { gql, useMutation } from '@apollo/client'
-import { CREATE_COOKBOOK } from '../../queries/Mutations'
+import { useMutation } from '@apollo/client';
+import { CREATE_COOKBOOK } from '../../queries/Mutations';
 
 const AddCookBook = ({ addNewBook, userId }) => {
   const [value, setValue] = useState('');
+  const [createBook, { data }] = useMutation(CREATE_COOKBOOK);
 
-  const [createBook, { data }] = useMutation(CREATE_COOKBOOK)
-
-  const handleSubmit =  (e) => {
+  const handleSubmit = e => {
     e.preventDefault();
     if (!value) return;
-    createBook({variables: {userId: userId, author:'Will be removed', title: value}})
-    .then(data => {
-      addNewBook(value, data.data.createCookbook.id)
-    })
+    createBook({
+      variables: { userId: userId, author: 'Will be removed', title: value },
+    }).then(data => {
+      addNewBook(value, data.data.createCookbook.id);
+    });
     setValue('');
   };
 
   return (
-    <form className='cookbook-form' onSubmit={handleSubmit}>
+    <form className="cookbook-form" onSubmit={handleSubmit}>
       <h3>Add a new cookbook</h3>
-      <label htmlFor='cookbook-name'></label>
+      <label htmlFor="cookbook-name"></label>
       <input
-        id='cookbook-name' 
-        type='text' 
-        className='new-book-input' 
-        placeholder='Cookbook name...' 
-        value={value} 
-        onChange={(e) => setValue(e.target.value)} 
+        id="cookbook-name"
+        type="text"
+        className="new-book-input"
+        placeholder="Cookbook name..."
+        value={value}
+        onChange={e => setValue(e.target.value)}
       />
-      <button type='submit'>Add to cookbooks</button>
+      <button type="submit">Add to cookbooks</button>
     </form>
   );
 };

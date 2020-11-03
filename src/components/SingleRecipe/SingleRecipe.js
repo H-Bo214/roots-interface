7892579f319b471909656900ed11a0a4410f6e2a
Tyler Haglund <relyt4me@gmail.com>
2020-11-02 19:27:01 -0700

    Update syntax per style guides for SingleRecipe

--- src/components/SingleRecipe/SingleRecipe.js
+++ src/components/SingleRecipe/SingleRecipe.js
@@ -1,34 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './SingleRecipe.css';
-import { useQuery } from '@apollo/client'
+import { useQuery } from '@apollo/client';
 import { GET_RECIPE } from '../../queries/Queries';
 
-const SingleRecipe = ({recipeId}) => {
-  const {loading, error, data} = useQuery(GET_RECIPE, {
+const SingleRecipe = ({ recipeId }) => {
+  const { loading, error, data } = useQuery(GET_RECIPE, {
     variables: { id: recipeId },
-  })
-
-  // map over ingredients
-  let recipeIngredients;
-  if(data) recipeIngredients = data.getRecipe.ingredients.map((ingredient) => {
-    return (
-      <li key={ingredient.id}>
-        {ingredient.amount} {ingredient.unit} {ingredient.name}
-      </li>
-    );
   });
 
-  if(loading) return <h1>Loading...</h1>;
-  if(error) {
+  let recipeIngredients;
+  if (data)
+    recipeIngredients = data.getRecipe.ingredients.map(ingredient => {
+      return (
+        <li key={ingredient.id}>
+          {ingredient.amount} {ingredient.unit} {ingredient.name}
+        </li>
+      );
+    });
+
+  if (loading) return <h1>Loading...</h1>;
+  if (error) {
     console.log(error);
     return <h1>error</h1>;
   }
 
   return (
-    <div className='recipe-page-background'>
-      <div className='recipe-page-paper'>
-        <article className='recipe-page-content'>
-          <div className='recipe-text'>
+    <div className="recipe-page-background">
+      <div className="recipe-page-paper">
+        <article className="recipe-page-content">
+          <div className="recipe-text">
             <h1>{data.getRecipe.title}</h1>
             <br></br>
             <p>{data.getRecipe.author}</p>
@@ -36,7 +36,7 @@ const SingleRecipe = ({recipeId}) => {
             <br></br>
             {recipeIngredients}
             <br></br>
-            <p className='recipe-instructions'>{data.getRecipe.instructions}</p>
+            <p className="recipe-instructions">{data.getRecipe.instructions}</p>
           </div>
         </article>
       </div>

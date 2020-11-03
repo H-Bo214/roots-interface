77693d4495c2aeb48a89312d514d89ad06e179c1
Tyler Haglund 2005FE <62810019+relyt4me@users.noreply.github.com>
2020-11-02 17:15:28 -0700

    Revert "Update syntax per style guides for Nav"
    This reverts commit e6e8bf8734e67340e14574936937e4cb4b07b330.

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom';
 import rootsLogo from '../../assets/roots-title-logo.jpg';
 import './Nav.css';
 
-const Nav = ({ user }) => {
-	return (
-		<nav>
-			<Link to="/">
-				<img src={rootsLogo} alt="logo with word roots" className="rootslogo" />
-			</Link>
-			<h2 className="nav-username">{user.name}</h2>
-		</nav>
-	);
+const Nav = ( { user }) => {
+  return (
+    <nav>
+      <Link to='/'>
+        <img src={rootsLogo} alt='logo with word roots' className='rootslogo' />
+      </Link>
+      <h2 className='nav-username'>{user.name}</h2>
+    </nav>
+  );
 };
 
 export default Nav;

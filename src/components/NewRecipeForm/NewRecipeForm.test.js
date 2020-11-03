14d8ae309ee055dc91d04356d947589d21d1b813
Tyler Haglund <relyt4me@gmail.com>
2020-11-03 12:00:00 -0700

    Add newRecipe render test

--- src/components/NewRecipeForm/NewRecipeForm.test.js
+++ src/components/NewRecipeForm/NewRecipeForm.test.js
@@ -3,84 +3,158 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import NewRecipeForm from '../NewRecipeForm/NewRecipeForm';
 import { MemoryRouter } from 'react-router-dom';
+import { MockedProvider } from '@apollo/client/testing';
+import { CREATE_RECIPE } from '../../queries/Mutations';
 
 describe('NewRecipeForm', () => {
+  let mocks;
   beforeEach(() => {
+    mocks = [
+      {
+        request: {
+          query: CREATE_RECIPE,
+          variables: {
+            description: 'recipe description',
+            instructions: 'recipe instructions',
+            title: 'recipe title',
+            author: 'recipe author',
+            ingredients: [{ name: 'flour', unit: 'cup', measurement: '1' }],
+            cookbookId: '1',
+          },
+        },
+        result: {
+          data: {
+            createCookbook: {
+              id: '1',
+            },
+          },
+        },
+      },
+    ];
+  });
+  it('should render all components of the page', () => {
     render(
       <MemoryRouter>
-        <NewRecipeForm />
+        <MockedProvider mocks={mocks}>
+          <NewRecipeForm user={{}} bookId="1" />
+        </MockedProvider>
       </MemoryRouter>,
     );
-  });
 
-  it('should render a heading', () => {
-    const h1 = screen.getByText('Add a recipe');
-    expect(h1).toBeInTheDocument();
-  });
-
-  it('should render a Recipe author input', () => {
+    const heading = screen.getByText('Add a recipe');
     const authorLabel = screen.getByText('Author:');
     const recipeAuthor = screen.getByPlaceholderText('Recipe author');
-    expect(authorLabel).toBeInTheDocument();
-    expect(recipeAuthor).toBeInTheDocument();
-  });
-
-  it('should render a Recipe name input', () => {
     const recipeLabel = screen.getByText('Recipe name:');
     const recipeName = screen.getByPlaceholderText('Recipe name');
-    expect(recipeLabel).toBeInTheDocument();
-    expect(recipeName).toBeInTheDocument();
-  });
-
-  it('should render a Recipe description input', () => {
     const descriptionLabel = screen.getByText('Description:');
     const recipeDescription = screen.getByPlaceholderText('Recipe description');
-    expect(recipeDescription).toBeInTheDocument();
-    expect(descriptionLabel).toBeInTheDocument();
-  });
-
-  it('should render Recipe instructions input', () => {
     const instructionsLabel = screen.getByText('Instructions:');
     const recipeInstructions = screen.getByPlaceholderText(
       'Recipe instructions',
     );
-    expect(recipeInstructions).toBeInTheDocument();
-    expect(instructionsLabel).toBeInTheDocument();
-  });
-
-  it('should render a Add to my recipe book button', () => {
-    const button = screen.getAllByRole('button');
-    expect(button[0]).toBeInTheDocument();
-  });
-
-  it('should render an Ingredient input', () => {
+    const submitButton = screen.getByRole('button', {
+      name: 'Add to my recipe book',
+    });
     const ingredientLabel = screen.getByText('Ingredient:');
     const ingredientName = screen.getByPlaceholderText('Ingredient name');
-    expect(ingredientName).toBeInTheDocument();
-    expect(ingredientLabel).toBeInTheDocument();
-  });
-
-  it('should render a unit input', () => {
     const unitLabel = screen.getByText('Unit:');
     const unit = screen.getByPlaceholderText('Unit');
-    expect(unit).toBeInTheDocument();
-    expect(unitLabel).toBeInTheDocument();
-  });
-
-  it('should render a measurement input', () => {
     const measurementLabel = screen.getByText('Measurement:');
     const measurement = screen.getByPlaceholderText('Measurement');
+    const addAdditionalIngredientButton = screen.getByRole('button', {
+      name: 'Ingredient +',
+    });
+    const removeIngredientFieldButton = screen.getByRole('button', {
+      name: 'Ingredient -',
+    });
+
+    expect(heading).toBeInTheDocument();
+    expect(authorLabel).toBeInTheDocument();
+    expect(recipeAuthor).toBeInTheDocument();
+    expect(recipeLabel).toBeInTheDocument();
+    expect(recipeName).toBeInTheDocument();
+    expect(recipeDescription).toBeInTheDocument();
+    expect(descriptionLabel).toBeInTheDocument();
+    expect(recipeInstructions).toBeInTheDocument();
+    expect(instructionsLabel).toBeInTheDocument();
+    expect(submitButton).toBeInTheDocument();
+    expect(ingredientName).toBeInTheDocument();
+    expect(ingredientLabel).toBeInTheDocument();
+    expect(unit).toBeInTheDocument();
+    expect(unitLabel).toBeInTheDocument();
     expect(measurement).toBeInTheDocument();
     expect(measurementLabel).toBeInTheDocument();
+    expect(addAdditionalIngredientButton).toBeInTheDocument();
+    expect(removeIngredientFieldButton).toBeInTheDocument();
   });
 
-  it('should render a Ingredient + button', () => {
-    const button = screen.getAllByRole('button');
-    expect(button[1]).toBeInTheDocument();
-  });
+  // it('should allow a user to add to the form inputs', () => {
+  //   render(
+  //     <MemoryRouter>
+  //       <MockedProvider mocks={mocks}>
+  //         <NewRecipeForm user={{}} bookId="1" />
+  //       </MockedProvider>
+  //     </MemoryRouter>,
+  //   );
 
-  it('should render a Ingredient - button', () => {
-    const button = screen.getAllByRole('button');
-    expect(button[2]).toBeInTheDocument();
-  });
+  //   const recipeAuthor = screen.getByPlaceholderText('Recipe author');
+  //   const recipeName = screen.getByPlaceholderText('Recipe name');
+  //   const recipeDescription = screen.getByPlaceholderText('Recipe description');
+  //   const recipeInstructions = screen.getByPlaceholderText(
+  //     'Recipe instructions',
+  //   );
+  //   const ingredientName = screen.getByPlaceholderText('Ingredient name');
+  //   const unit = screen.getByPlaceholderText('Unit');
+  //   const measurement = screen.getByPlaceholderText('Measurement');
+
+  //   fireEvent.change(recipeAuthor, { target: { value: 'recipe author' } });
+  //   fireEvent.change(recipeName, { target: { value: 'recipe title' } });
+  //   fireEvent.change(recipeDescription, {
+  //     target: { value: 'recipe description' },
+  //   });
+  //   fireEvent.change(recipeInstructions, {
+  //     target: { value: 'recipe instructions' },
+  //   });
+  //   fireEvent.change(ingredientName, { target: { value: 'flour' } });
+  //   fireEvent.change(unit, { target: { value: 'cup' } });
+  //   fireEvent.change(measurement, { target: { value: '1' } });
+
+  //   expect(recipeAuthor.value).toBe('recipe author');
+  //   expect(recipeName.value).toBe('recipe title');
+  //   expect(recipeDescription.value).toBe('recipe description');
+  //   expect(recipeInstructions.value).toBe('recipe instructions');
+  //   expect(ingredientName.value).toBe('flour');
+  //   expect(unit.value).toBe('cup');
+  //   expect(measurement.value).toBe('1');
+  // });
+
+  // it('should allow a user to add additional ingredient fields', () => {
+  //   render(
+  //     <MemoryRouter>
+  //       <MockedProvider mocks={mocks}>
+  //         <NewRecipeForm user={{}} bookId="1" />
+  //       </MockedProvider>
+  //     </MemoryRouter>,
+  //   );
+
+  //   const addAdditionalIngredientButton = screen.getByRole('button', {
+  //     name: 'Ingredient +',
+  //   });
+
+  //   fireEvent.click(addAdditionalIngredientButton);
+
+  //   const ingredientLabels = screen.getAllByText('Ingredient:');
+  //   const ingredientNames = screen.getAllByPlaceholderText('Ingredient name');
+  //   const unitLabels = screen.getAllByText('Unit:');
+  //   const units = screen.getAllByPlaceholderText('Unit');
+  //   const measurementLabels = screen.getAllByText('Measurement:');
+  //   const measurements = screen.getAllByPlaceholderText('Measurement');
+
+  //   expect(ingredientNames.length).toEqual(2);
+  //   expect(ingredientLabels.length).toEqual(2);
+  //   expect(units.length).toEqual(2);
+  //   expect(unitLabels.length).toEqual(2);
+  //   expect(measurements.length).toEqual(2);
+  //   expect(measurementLabels.length).toEqual(2);
+  // });
 });

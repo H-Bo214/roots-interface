f44866fd47503f8ddc4cfd8719b362650cf8bb43
Horacio Borrego <hborrego365@gmail.com>
2020-11-03 16:48:39 -0700

    Add 1 test to App.test.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -36,6 +36,7 @@ const App = () => {
 
   return (
     <div className="App">
+      {console.log('data', data)}
       <Switch>
         <Route
           path="/recipe-book/:bookId"
--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,13 +1,74 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, findByTestId, waitFor, findByText } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from '../App/App';
 import { MemoryRouter } from 'react-router-dom';
 import { MockedProvider } from '@apollo/client/testing';
-import { CREATE_COOKBOOK } from '../../queries/Mutations';
+import { GET_USER, GET_USER_COOKBOOKS} from '../../queries/Queries';
+
+const waitForData = () => new Promise(res => setTimeout(res, 0))
 
 describe('App', () => {
-  it('should pass a test', () => {
-    expect(true).toEqual(true);
+  let mockUser, mockGetCookbooks
+  beforeEach(() => {
+    mockUser = [
+      {
+        request: {
+          query: GET_USER,
+          variables: {
+            id: '6'
+          },
+        },
+        result: {
+          data: {
+            getUser: {
+              email: "tyler@creatorFam.com",
+              id: "6",
+              name: "Tyler",
+            },
+          },
+        },
+      },
+    ];
   });
+
+  mockGetCookbooks = [
+    {
+      request: {
+        query: GET_USER_COOKBOOKS,
+        variables: {
+          id: '6',
+        },
+      },
+      result: {
+        data: {
+          getUserCookbooks: [
+            {
+              id: '1',
+              title: "Neeru's Cookbook",
+            },
+            {
+              id: '2',
+              title: 'Indian Food',
+            },
+          ],
+        },
+      },
+    },
+  ];
+
+  it('should render loading state initially', async () => {
+    await waitForData()
+    render(
+      <MemoryRouter>
+        <MockedProvider mocks={mockUser}>
+          <App />
+        </MockedProvider>
+      </MemoryRouter>,
+    );
+
+    const loading = screen.getByText('Loading...');
+    expect(loading).toBeInTheDocument();
+  });
+  
 });

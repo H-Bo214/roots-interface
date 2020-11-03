ec78c5a74e3d8059b516caf5cadc7bfafe8a6cd5
Tyler Haglund <relyt4me@gmail.com>
2020-11-02 19:29:49 -0700

    Update syntax per style guides for index

--- src/index.js
+++ src/index.js
@@ -1,31 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {ApolloProvider, ApolloClient, InMemoryCache} from '@apollo/client'
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
-//https://cors-anywhere.herokuapp.com/
 const client = new ApolloClient({
   uri: 'https://glacial-falls-21490.herokuapp.com/graphql',
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-// https://cors-anywhere.herokuapp.com/

0f22081c6174ebdeee7d1ccc53aeedf279a09cf5
Tyler Haglund 2005FE <62810019+relyt4me@users.noreply.github.com>
2020-11-02 17:15:28 -0700

    Revert "Update syntax per style guides for AddRecipe"
    This reverts commit 185d9cfb981cd031a76c524e62228422d5a67a9b.

--- src/components/AddRecipe/AddRecipe.js
+++ src/components/AddRecipe/AddRecipe.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from 'react'
 // import AddRecipeFromBook from '../AddRecipeFromBook/AddRecipeFromBook';
 import NewRecipeForm from '../NewRecipeForm/NewRecipeForm';
-import './AddRecipe.css';
+import './AddRecipe.css'
 
 const AddRecipe = ({ user, bookId }) => {
-	return (
-		<div className="AddRecipe">
-			{/* Will need to render this component once we implement this feature. See the AddRecipeFromBook component file */}
-			{/* <AddRecipeFromBook user={user} /> */}
-			<NewRecipeForm user={user} bookId={bookId} />
-		</div>
-	);
-};
+  return (
+    <div className='AddRecipe'>
+      {/* Will need to render this component once we implement this feature. See the AddRecipeFromBook component file */}
+      {/* <AddRecipeFromBook user={user} /> */}
+      <NewRecipeForm user={user} bookId={bookId} />
+    </div>
+  )
+}
 
 export default AddRecipe;

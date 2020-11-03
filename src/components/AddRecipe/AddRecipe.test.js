3f549863ab879c32589897e7210caadeffcece72
Tyler Haglund <relyt4me@gmail.com>
2020-11-03 10:57:03 -0700

    Add test for AddRecipe container component

--- src/components/AddRecipe/AddRecipe.test.js
+++ src/components/AddRecipe/AddRecipe.test.js
@@ -1,5 +1,23 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import AddRecipe from './AddRecipe';
+import { MemoryRouter } from 'react-router-dom';
+
 describe('AddRecipe', () => {
-  it('should pass a test', () => {
-    expect(true).toEqual(true);
+  it('should render the basic areas of the container', () => {
+    render(
+      <MemoryRouter>
+        <AddRecipe />
+      </MemoryRouter>,
+    );
+
+    const h1 = screen.getByText('Add a recipe');
+    const authorLabel = screen.getByText('Author:');
+    const recipeAuthor = screen.getByPlaceholderText('Recipe author');
+
+    expect(h1).toBeInTheDocument();
+    expect(authorLabel).toBeInTheDocument();
+    expect(recipeAuthor).toBeInTheDocument();
   });
 });

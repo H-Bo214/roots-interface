79772518bef73570ec22913f646866b43deac444
Tyler Haglund 2005FE <62810019+relyt4me@users.noreply.github.com>
2020-11-02 17:15:28 -0700

    Revert "Update syntax per style guides for RecipeBook"
    This reverts commit 9a7563d121c204a3152c82c494234f1d1186c17e.

--- src/components/RecipeBook/RecipeBook.js
+++ src/components/RecipeBook/RecipeBook.js
@@ -1,49 +1,48 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './RecipeBook.css';
-import { useQuery } from '@apollo/client';
-import { GET_COOKBOOK } from '../../queries/Queries';
+import { useQuery } from '@apollo/client'
+import { GET_COOKBOOK } from '../../queries/Queries'
 
-const RecipeBook = ({ bookId }) => {
-	const { loading, error, data } = useQuery(GET_COOKBOOK, {
-		variables: { id: bookId },
-	});
+const RecipeBook = ( { bookId }) => {
+  const {loading, error, data} = useQuery(GET_COOKBOOK, {
+    variables: { id: bookId },
+  })
 
-	let listOfRecipes;
-	if (data)
-		listOfRecipes = data.getCookbook.recipes.map((recipe) => {
-			return (
-				<Link to={`/single-recipe/${recipe.id}`} key={recipe.id}>
-					<li>{recipe.title}</li>
-				</Link>
-			);
-		});
+  let listOfRecipes;
+  if(data) listOfRecipes = data.getCookbook.recipes.map((recipe) => {
+    return (
+      <Link to={`/single-recipe/${recipe.id}`} key={recipe.id}>
+        <li>{recipe.title}</li>
+      </Link>
+    );
+  });
 
-	if (loading) return <h1>Loading...</h1>;
-	if (error) {
-		console.log(error);
-		return <h1>error</h1>;
-	}
+  if(loading) return <h1>Loading...</h1>;
+  if(error) {
+    console.log(error);
+    return <h1>error</h1>;
+  }
 
-	return (
-		<div className="recipe-book-page">
-			<div className="recipe-book-wrapper">
-				<section className="recipe-book">
-					<div className="left-page">
-						<h1 className="book-title">{data.getCookbook.title}</h1>
-					</div>
-					<div className="right-page">
-						<p className="author">Hosted by: {data.getCookbook.author}</p>
-						<h3 className="right-page-title">Recipes:</h3>
-						<article className="recipe-list">{listOfRecipes}</article>
-						<Link to={`/add-recipe/${bookId}`}>
-							<button>Add Recipe</button>
-						</Link>
-					</div>
-				</section>
-			</div>
-		</div>
-	);
+  return (
+    <div className='recipe-book-page'>
+      <div className='recipe-book-wrapper'>
+        <section className='recipe-book'>
+          <div className='left-page'>
+            <h1 className='book-title'>{data.getCookbook.title}</h1>
+          </div>
+          <div className='right-page'>
+            <p className='author'>Hosted by: {data.getCookbook.author}</p>
+            <h3 className='right-page-title'>Recipes:</h3>
+            <article className='recipe-list'>{listOfRecipes}</article>
+            <Link to={`/add-recipe/${bookId}`}>
+              <button>Add Recipe</button>
+            </Link>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
 };
 
 export default RecipeBook;

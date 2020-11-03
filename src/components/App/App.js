62f6548ae6778e88c6552509f3ee982cbd6ad8af
Tyler Haglund <relyt4me@gmail.com>
2020-11-02 18:38:51 -0700

    Format styling to match dogs App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,66 +1,66 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Nav from '../Nav/Nav';
 import BookTree from '../BookTree/BookTree';
 import RecipeBook from '../RecipeBook/RecipeBook';
 import SingleRecipe from '../SingleRecipe/SingleRecipe';
-import { useQuery} from '@apollo/client'
+import { useQuery } from '@apollo/client';
 import './App.css';
 import AddRecipe from '../AddRecipe/AddRecipe';
-import { GET_USER } from '../../queries/Queries'
-
-
+import { GET_USER } from '../../queries/Queries';
 
 const App = () => {
-  const id = '1' 
-  const { loading, error, data } = useQuery(GET_USER, { variables: {id} })
+  const id = '1';
+  const { loading, error, data } = useQuery(GET_USER, { variables: { id } });
 
-  const [user, setUser] = useState({ email: '', id: '', name: '' })
+  const [user, setUser] = useState({ email: '', id: '', name: '' });
 
   useEffect(() => {
-    if(data) {
+    if (data) {
       let foundUser = {
         email: data.getUser.email,
         id: data.getUser.id,
         name: data.getUser.name,
-      }
-      setUser(foundUser)
+      };
+      setUser(foundUser);
     }
-  }, [data])
+  }, [data]);
 
-  if(loading) return <h1>Loading...</h1>;
-  if(error) {
+  if (loading) return <h1>Loading...</h1>;
+  if (error) {
     console.log(error);
-    return <h1>An error occurred getting your information. Please try again.</h1>;
+    return (
+      <h1>An error occurred getting your information. Please try again.</h1>
+    );
   }
 
   return (
-    <div className='App'>
+    <div className="App">
       <Switch>
         <Route
-          path='/recipe-book/:bookId'
+          path="/recipe-book/:bookId"
           render={({ match }) => {
-            return <RecipeBook  bookId={match.params.bookId} />;
+            return <RecipeBook bookId={match.params.bookId} />;
           }}
         />
         <Route
-          path='/add-recipe/:bookId'
+          path="/add-recipe/:bookId"
           render={({ match }) => {
             return <AddRecipe user={user} bookId={match.params.bookId} />;
           }}
         />
         <Route
-          path='/single-recipe/:recipeId'
+          path="/single-recipe/:recipeId"
           render={({ match }) => {
-            return <SingleRecipe recipeId={match.params.recipeId}/>;
+            return <SingleRecipe recipeId={match.params.recipeId} />;
           }}
         />
-        <Route 
-          path='/'
-          render={ () => {
-           return <BookTree user={user} />
-          }} 
-           />
+        <Route
+          path="/"
+          render={() => {
+            return <BookTree user={user} />;
+          }}
+        />
       </Switch>
       <Nav user={user} />
     </div>

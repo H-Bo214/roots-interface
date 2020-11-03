6da1a146ba2e8902af88e6c7155be16392ad4fa2
Tyler Haglund 2005FE <62810019+relyt4me@users.noreply.github.com>
2020-11-02 17:15:28 -0700

    Revert "Update syntax per style guides for RecipeBook"
    This reverts commit 9a7563d121c204a3152c82c494234f1d1186c17e.

--- src/components/RecipeBook/RecipeBook.test.js
+++ src/components/RecipeBook/RecipeBook.test.js
@@ -2,77 +2,82 @@ import React from 'react';
 import { screen, render, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import RecipeBook from './RecipeBook';
-import { GET_COOKBOOK } from '../../queries/Queries';
-import { MockedProvider } from '@apollo/client/testing';
+import { GET_COOKBOOK } from '../../queries/Queries'
+import { MockedProvider} from '@apollo/client/testing'
+
 
 describe('Recipe Book', () => {
-	let mocks;
-	beforeEach(() => {
-		mocks = [
-			{
-				request: {
-					query: GET_COOKBOOK,
-					variables: {
-						id: '1',
-					},
-				},
-				result: {
-					data: {
-						getCookbook: {
-							author: 'Michael Alex',
-							id: '1',
-							title: "Michael's Cookbook",
-							recipes: [
-								{
-									title: 'Beef Stew',
-									id: '1',
-								},
-								{
-									title: 'Chowder',
-									id: '2',
-								},
-							],
-						},
-					},
-				},
-			},
-		];
-	});
-	it('should render with loading while loading', () => {
-		render(
-			<MemoryRouter>
-				<MockedProvider mocks={mocks}>
-					<RecipeBook bookId={'1'} />
-				</MockedProvider>
-			</MemoryRouter>
-		);
+  let mocks;
+  beforeEach(() => {
+    mocks = [
+      {
+        request: {
+          query: GET_COOKBOOK,
+          variables: {
+            id: '1',
+          },
+        },
+        result: {
+          data: {
+            "getCookbook": {
+              "author": "Michael Alex",
+              "id": "1",
+              "title": "Michael's Cookbook",
+              "recipes": [
+                {
+                  "title": "Beef Stew",
+                  "id": "1"
+                },
+                {
+                  "title": "Chowder",
+                  "id": "2"
+                }
+              ],
+            }
+          }
+        },
+      },
+    ]
+  })
+  it('should render with loading while loading', () => {
+    render(
+      <MemoryRouter>
+        <MockedProvider mocks={mocks}>
+          <RecipeBook
+            bookId={'1'}
+          />
+        </MockedProvider>
+      </MemoryRouter>
+    );
 
-		const loading = screen.getByText('Loading...');
+    const loading = screen.getByText('Loading...')
 
-		expect(loading).toBeInTheDocument();
-	});
+    expect(loading).toBeInTheDocument();
+  });
 
-	it('should render correctly after loading', async () => {
-		render(
-			<MemoryRouter>
-				<MockedProvider mocks={mocks}>
-					<RecipeBook bookId={'1'} />
-				</MockedProvider>
-			</MemoryRouter>
-		);
+  it('should render correctly after loading', async () => {
+    render(
+      <MemoryRouter>
+        <MockedProvider mocks={mocks}>
+          <RecipeBook
+            bookId={'1'}
+          />
+        </MockedProvider>
+      </MemoryRouter>
+    );
 
-		const title = await waitFor(() => screen.getByText("Michael's Cookbook"));
-		const author = screen.getByText('Hosted by: Michael Alex');
-		const recipesTitle = screen.getByText('Recipes:');
-		const recipeOne = screen.getByText('Beef Stew');
-		const recipeTwo = screen.getByText('Chowder');
-		const addRecipeButton = screen.getByRole('button', { name: 'Add Recipe' });
+    const title = await waitFor(() => screen.getByText('Michael\'s Cookbook'));
+    const author = screen.getByText('Hosted by: Michael Alex');
+    const recipesTitle = screen.getByText('Recipes:');
+    const recipeOne = screen.getByText('Beef Stew');
+    const recipeTwo = screen.getByText('Chowder');
+    const addRecipeButton = screen.getByRole('button', {name: 'Add Recipe'});
 
-		expect(title).toBeInTheDocument();
-		expect(author).toBeInTheDocument();
-		expect(recipesTitle).toBeInTheDocument();
-		expect(recipeOne).toBeInTheDocument();
-		expect(recipeTwo).toBeInTheDocument();
-		expect(addRecipeButton).toBeInTheDocument();
-	});
+    expect(title).toBeInTheDocument();
+    expect(author).toBeInTheDocument();
+    expect(recipesTitle).toBeInTheDocument();
+    expect(recipeOne).toBeInTheDocument();
+    expect(recipeTwo).toBeInTheDocument();
+    expect(addRecipeButton).toBeInTheDocument();
+  });
 });
